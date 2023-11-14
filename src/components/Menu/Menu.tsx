85c65e369c8eba5fd332e1627f82f016ce65e36c
Fakhirazzahrah Ramadhani <fzzahrahh@gmail.com>
2023-11-14 15:12:32 +0800

    Publish List User

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
-import { homeOutline, newspaperOutline, logOutOutline, settingsOutline, personCircleOutline } from 'ionicons/icons';
+import { homeOutline, newspaperOutline, logOutOutline, settingsOutline, personCircleOutline, contractOutline, personCircle, listCircleOutline } from 'ionicons/icons';
 import Home from '../../pages/Home';
 import Setting from '../../pages/Setting';
 import Details from '../../pages/Details';
 import NewsComponent from '../News/News';
 import Profile from '../../pages/Profile';
 import News from '../../pages/News';
+import ListUser from '../../pages/ListUser';
 import './Menu.css'
+
 
 const Menu: React.FC = () => {
 
   const paths = [
     { name: 'Home', url: '/app/home', icon: homeOutline },
     { name: 'Profile', url: '/app/profile', icon: personCircleOutline},
+    { name: 'User List', url: '/app/UserList',icon:listCircleOutline},
     { name: 'News', url: '/app/news', icon: newspaperOutline}
+   
   ];
   return (
       <IonPage>
@@ -53,6 +57,7 @@ const Menu: React.FC = () => {
             <Route path='/app/profile' component={Profile} />
             <Route path='/app/news' component={News} />
             <Route path='/app/home/details' component={Details} />
+            <Route path='/app/UserList' component={ListUser}/>
             <Route exact path='/app'>
               <Redirect to='/app/home' />
             </Route>
